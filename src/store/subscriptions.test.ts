6efffac6fb8cad0c73f00f1f6894198ccc22ee3c
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2019-02-24 15:58:04 +0100

    removed redundant tests

--- src/store/subscriptions.test.ts
+++ src/store/subscriptions.test.ts
@@ -4,7 +4,7 @@ import { players } from '../types';
 import { createStore, playersTime$ } from './';
 import { clockTimerSubscription } from './subscriptions';
 import { TICK_INTERVAL } from '../constants';
-import { toggleClock } from './actions';
+import { toggleClock, pauseClock } from './actions';
 import { oppositePlayer } from '../helpers';
 
 jest.useFakeTimers();
@@ -53,15 +53,16 @@ describe('clockTimerSubscription', () => {
 
 
   describe('given the clock is running', () => {
-    for (let player of players) {
-      describe(`and ${player} is to move`, () => {
-        const elapsedTime = 500;
-        let playerToMoveTime: number, oppositePlayerTime: number;
+    for (let playerToMove of players) {
+      describe(`and ${playerToMove} is to move`, () => {
+        const elapsedTime = 5 * TICK_INTERVAL;
+        const playersTime: any = {};
 
         beforeEach(() => {
-          store.dispatch(toggleClock(oppositePlayer(player), getCurrentTime));
-          playerToMoveTime = playersTime$(store.getState(), player);
-          oppositePlayerTime = playersTime$(store.getState(), oppositePlayer(player));
+          store.dispatch(toggleClock(oppositePlayer(playerToMove), getCurrentTime));
+          for (let player of players) {
+            playersTime[player] = playersTime$(store.getState(), player);
+          }
         });
 
         describe(`when ${elapsedTime}ms elapse`, () => {
@@ -69,23 +70,38 @@ describe('clockTimerSubscription', () => {
             advanceTimeBy(elapsedTime);
           });
 
-          it(`${player}'s time is decreased correctly`, () => {
+          it(`${playerToMove}'s time is decreased correctly`, () => {
             expect(
-              playersTime$(store.getState(), player)
-            ).toEqual(playerToMoveTime - elapsedTime);
+              playersTime$(store.getState(), playerToMove)
+            ).toEqual(playersTime[playerToMove] - elapsedTime);
           });
 
-          it(`${oppositePlayer(player)}'s time is unchanged`, () => {
+          it(`${oppositePlayer(playerToMove)}'s time is unchanged`, () => {
             expect(
-              playersTime$(store.getState(), oppositePlayer(player))
-            ).toEqual(oppositePlayerTime);
+              playersTime$(store.getState(), oppositePlayer(playerToMove))
+            ).toEqual(playersTime[oppositePlayer(playerToMove)]);
           });
 
-          describe('TODO and the clock is stopped', () => {
-            describe('TODO and some more time elapses', () => {
-              it.skip('TODO both players\' time is unchanged', () => {
-                expect(true).toBe(false);
+          describe('and the clock is paused', () => {
+            beforeEach(() => {
+              store.dispatch(pauseClock(getCurrentTime));
+            });
+
+            describe(`and another ${elapsedTime}ms elapse`, () => {
+              beforeEach(() => {
+                advanceTimeBy(elapsedTime);
               });
+
+              for (let player of players) {
+                it(`${player}'s time is unchanged`, () => {
+                  const expectedTime = (player === playerToMove)
+                    ? playersTime[player] - elapsedTime
+                    : playersTime[player];
+                  expect(
+                    playersTime$(store.getState(), player)
+                  ).toBe(expectedTime);
+                });
+              }
             });
           });
         });
